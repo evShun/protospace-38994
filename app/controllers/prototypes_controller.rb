9d741fdd60c53d77ed5af54f888e10a3380ee44b
evShun <124552406+evShun@users.noreply.github.com>
2023-02-22 18:27:16 +0900

    install ActiveRecord &  valitated

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,5 +2,10 @@ class PrototypesController < ApplicationController
   
   def index
   end
-  
+
+  private
+
+  def prototype_params
+    #  params.require(:prototype).permit(:image, :title, :catch_copy, :concept,).merge(user_id: current_user.id)
+  end
 end
