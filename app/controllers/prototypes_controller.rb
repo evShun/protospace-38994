003bd4d767299d64b5a679bbc2d6301641532f5e
evShun <124552406+evShun@users.noreply.github.com>
2023-02-27 17:15:08 +0900

    完成！

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
-  
+  before_action :authenticate_user!,only: [:new,:delete,]
+  before_action :move_to_index, only: :edit
+
   def index
     @prototypes = Prototype.all
   end
@@ -19,6 +21,8 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    @comment   = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
@@ -44,5 +48,11 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:image, :title, :catch_copy, :concept,).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == params[:id]
+      redirect_to action: :index
+    end
   end
 end
